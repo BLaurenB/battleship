2da5a002608ed423ab7186d41bbc4783fa1db8a1
BLaurenB <blbillington1@gmail.com>
2017-09-02 18:55:45 -0600

    added initial UI with clear-screen

---
+++ lib/user_communication.rb
@@ -0,0 +1,59 @@
+class UI
+
+  attr_accessor :puts_variable
+
+#initiate timer -- when?
+
+  def initialize
+    #all the things
+    @puts_variable
+
+  end
+
+  def cheat
+    #if user_input == "c"
+    #show computer's board...but mayeb this should be in the board class?
+  end
+
+
+  def instructions
+    #may need to be split out into different methods for different parts.
+  end
+
+  def welcome
+  end
+
+
+  def ui_initialize
+    @puts_variable =  "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    user_input = gets.chomp
+
+    if user_input.downcase == "p"
+      #stuff
+    elsif user_input.downcase == "i"
+      puts instructions
+    elsif user_input ==
+      #stuff
+    else
+      @puts_variable = "Please try again.\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    end
+  end
+
+
+
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  #initialize classes here
+  ui = UI.new
+  print "\e[H\e[2J"
+  puts "\n\n\n\nWelcome to BATTLESHIP\n\n\n\nPress Enter to begin" #for some sort of time period?
+  user_input = gets
+  print "\e[H\e[2J"
+
+  puts "#{@puts_variable}"
+
+
+end
